6df09cb4cf9503fa26f46a845457953e3d146568
Jim M. R. Teichgräber <jim.teichgraeber@jetbrains.com>
2026-01-23 14:21:00 +0100

    start on sticky shift overloading of nav layer key

--- shared.h
+++ shared.h
@@ -8,28 +8,39 @@
 #define MAGIC 3
 #define UMLAUT 4
 
-#define SHARED_HRM_COMPONENTS                 \
+#define SHARED_MODTAP_COMPONENTS                 \
         compatible = "zmk,behavior-hold-tap"; \
-        #binding-cells = <2>;                 \
         flavor = "balanced";                  \
         tapping-term-ms = <280>;              \
         quick-tap-ms = <175>;                 \
         require-prior-idle-ms = <60>;         \
-        bindings = <&kp>, <&kp>;              \
         hold-trigger-on-release;
 
 #define HRM_BEHAVIORS                                                   \
         hrms_l: hrms_l_behavior {                                       \
-            SHARED_HRM_COMPONENTS                                       \
+            SHARED_MODTAP_COMPONENTS                                       \
+            #binding-cells = <2>;                 \
+            bindings = <&kp>, <&kp>;              \
             hold-trigger-key-positions = <KEYS_MAIN_R KEYS_THUMBS_ALL>; \
         };                                                              \
         hrms_r: hrms_r_behavior {                                       \
-            SHARED_HRM_COMPONENTS                                       \
+            SHARED_MODTAP_COMPONENTS                                       \
+            #binding-cells = <2>;                 \
+            bindings = <&kp>, <&kp>;              \
             hold-trigger-key-positions = <KEYS_MAIN_L KEYS_THUMBS_ALL>; \
         };
 
+#define NAV_LAYER_STICKY_SHIFT_BEHAVIOR\
+        nav_sticky_shift: nav_sticky_shift_behavior { \
+            SHARED_MODTAP_COMPONENTS \
+            #binding-cells = <0>;                 \
+            bindings = <&mo NAV>, <&sk LSHFT>;              \
+            hold-trigger-key-positions = <KEYS_MAIN_ALL>; \
+        };
+        
+
 #define DEFAULT_HOME_ROW_MAP \
-        &hrms_l LCTRL ESC        &hrms_l LGUI A    &hrms_l LALT S       &hrms_l LCTRL D       &hrms_l LSHIFT F       &kp G                               &kp H   &hrms_r RSHIFT J   &hrms_r RCTRL K       &hrms_r LALT L       &hrms_r RGUI SEMI    &lt UMLAUT SQT
+        &hrms_l LCTRL ESC        &hrms_l LGUI A    &hrms_l LALT S       &hrms_l LCTRL D       &hrms_l LSHFT F       &kp G                               &kp H   &hrms_r RSHFT J   &hrms_r RCTRL K       &hrms_r LALT L       &hrms_r RGUI SEMI    &lt UMLAUT SQT
         
 
 #endif // SHARED_H
